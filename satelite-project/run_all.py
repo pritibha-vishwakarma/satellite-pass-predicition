0ca95090b940e081c5218fb9da61498e53883086
pritibha-vishwakarma <166431771+pritibha-vishwakarma@users.noreply.github.com>
2026-03-26 17:26:02 +0530

    Update run_all.py

--- satelite-project/run_all.py
+++ satelite-project/run_all.py
@@ -1,15 +1,12 @@
 import subprocess
 import time
 
-print("🚀 Starting Project...")
+print(" Starting Project...")
 
-# Step 1: Run main.py
-print("📡 Running main.py (data processing)...")
+print(" Running main.py (data processing)...")
 subprocess.run(["python", "main.py"])
 
-# Small delay
 time.sleep(2)
 
-# Step 2: Start API
 print("🌐 Starting API server...")
 subprocess.run(["uvicorn", "api:app", "--reload"])
